dcc18e28f3cd9dea1f3a375467864aa9a05c762a
aboyd52501 <aboyd52501@gmail.com>
2022-07-02 11:54:18 -0500

    1.7 unicafe step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const average = ((good - bad) / total) || 0; // || 0 -> Prevent NaN from rendering
+  const positive = (good / total) || 0;
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -15,6 +19,9 @@ const App = () => {
       <DisplayCounter name='good' value={good} />
       <DisplayCounter name='neutral' value={neutral} />
       <DisplayCounter name='bad' value={bad} />
+      <DisplayCounter name="all" value={total} />
+      <DisplayCounter name="average" value={average} />
+      <DisplayCounter name="positive" value={positive} />
     </div>
   );
 };
