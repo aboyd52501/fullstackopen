a4d68f1568b9525f5876b1041e65174407170f31
aboyd52501 <aboyd52501@gmail.com>
2022-07-03 11:50:43 -0500

    2.6 the phonebook step 1: basic form working

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,40 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      id: 1
+    }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      id: crypto.randomUUID()
+    };
+    setPersons([...persons, newPerson]);
+    setNewName('');
+  };
+
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.id}>{person.name}</p>)}
     </div>
   )
 }
