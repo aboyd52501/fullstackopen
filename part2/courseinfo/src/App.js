5cb36235e1414d523ac361f15068d8133a38d778
aboyd52501 <aboyd52501@gmail.com>
2022-07-03 11:16:28 -0500

    2.5 separate module: separated Course module from App.js

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,28 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  parts.map(part =>
-    <Part key={part.id} part={part} />
-  );
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={total} />
-    </div>
-  );
-}
+import Course from './components/course'
 
 const App = () => {
   const courses = [
