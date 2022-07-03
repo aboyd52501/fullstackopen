178311cd11b26ece1970e7fb76f05dfeef782438
aboyd52501 <aboyd52501@gmail.com>
2022-07-03 11:59:16 -0500

    2.8 the phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,13 +5,20 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
+      number: '1-111-111-1111',
       id: 1
     },
-  ]) 
-  const [newName, setNewName] = useState('')
+  ]);
+  
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -21,14 +28,16 @@ const App = () => {
       alert(`${newName} is already in the phonebook!`);
     }
     else {
-        const newPerson = {
+      const newPerson = {
         name: newName,
+        number: newNumber,
         id: crypto.randomUUID()
       };
       setPersons([...persons, newPerson]);
     }
     
     setNewName('');
+    setNewNumber('');
 
   };
 
@@ -40,11 +49,14 @@ const App = () => {
           name: <input value={newName} onChange={handleName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+      {persons.map(person => <p key={person.id}>{person.name}: {person.number}</p>)}
     </div>
   )
 }
