ce207c91dcd4bb3482cdcf23233b3dc2bff1d26d
aboyd52501 <aboyd52501@gmail.com>
2022-08-14 11:30:25 -0500

    5.1 bloglist frontend step 1

--- part5/bloglist-frontend-main/src/App.js
+++ part5/bloglist-frontend-main/src/App.js
@@ -1,9 +1,12 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +14,38 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  const submitLogin = loginData => {
+    axios
+      .post('/api/login', loginData)
+      .then(res => {
+        const { data } = res;
+        setUser(data)
+        console.log(data)
+      })
+      .catch(err => {
+        if (err.response.status === 401)
+          alert('Invalid Credentials');
+      });
+  };
+
+  if (user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} ({user.username}) logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <LoginForm submitLogin={submitLogin}/>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ part5/bloglist-frontend-main/src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const makeHandler = setFxn => e => setFxn(e.target.value);
+
+const LoginForm = ({ submitLogin }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsername = makeHandler(setUsername)
+  const handlePassword = makeHandler(setPassword)
+
+  const handleLogin = e => {
+    e.preventDefault()
+
+    const loginData = {
+      username,
+      password,
+    }
+
+    submitLogin(loginData);
+
+    setUsername('')
+    setPassword('')
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+            <label htmlFor='username'>username: </label>
+            <input id='username' type='text' value={username} onChange={handleUsername} />
+        </div>
+        <div>
+            <label htmlFor='password'>password: </label>
+            <input id='password' type='password' value={password} onChange={handlePassword} />
+        </div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
+  )
+};
+
+export default LoginForm
