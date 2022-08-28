dc3b9f464e5c1260c62c93c7f5a2f4892acbd6bb
aboyd52501 <aboyd52501@gmail.com>
2022-08-28 13:07:19 -0500

    5.7 Blog list frontend step 7
    Added button to expand details for each blog, added styling.

--- part5/bloglist-frontend-main/src/components/Blog.js
+++ part5/bloglist-frontend-main/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author} ({blog.user.username})
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    margin: '8px 0 8px 0',
+    border: '1px solid',
+    padding: 8,
+  }
+
+  const [open, setOpen] = useState()
+
+  const toggleVisibility = () => setOpen(!open)
+
+  const hideOpen = {
+    display: open ? 'none' : '',
+  }
+
+  const showOpen = {
+    display: open ? '' : 'none',
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} ({blog.user.username}) {' '}
+      <button style={hideOpen} onClick={toggleVisibility}>View</button>
+      <button style={showOpen} onClick={toggleVisibility}>Hide</button>
+      <div style={showOpen}>
+        <ul>
+          <li>{blog.url}</li>
+          <li>likes: {blog.likes} <button>like</button></li>
+          <li>{blog.author}</li>
+        </ul>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
