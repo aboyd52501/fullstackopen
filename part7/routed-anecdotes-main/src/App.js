51c0eb8efdcbb1ee3be19ece60811de718f2822b
unknown <aboyd52501@gmail.com>
2023-07-07 08:08:28 -0500

    7.1 - Implemented React Router for viewing application pages

--- part7/routed-anecdotes-main/src/App.js
+++ part7/routed-anecdotes-main/src/App.js
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/anecdotes' style={padding}>anecdotes</Link>
+      <Link to='/create' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -123,14 +127,20 @@ const App = () => {
   }
 
   return (
-    <div>
+    <Router>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
+      {/* <AnecdoteList anecdotes={anecdotes} />
       <About />
-      <CreateNew addNew={addNew} />
+      <CreateNew addNew={addNew} /> */}
+      <Routes>
+        <Route path='/anecdotes' element={<AnecdoteList anecdotes={anecdotes}/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/create' element={<CreateNew addNew={addNew} />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   )
 }
 
